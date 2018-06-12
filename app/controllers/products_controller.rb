775f230a3252bde8a2cf9fb69c7035dd9568ffae
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-12 16:44:28 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create(product_params)
     render :json => @product, :status => 201
-    redirect_to products_path
+    # redirect_to products_path
   end
 
   def show
